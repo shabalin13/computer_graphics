5b0ccb0eeabd7d82201522825fa75754a2cdc86e
Shabalin13 <d.shabalin@innopolis.university>
2022-09-22 22:45:33 +0300

    Implement set_render_target, set_viewport, clear_render_target methods of cg::renderer::rasterizer class

--- src/renderer/rasterizer/rasterizer.h
+++ src/renderer/rasterizer/rasterizer.h
@@ -2,12 +2,11 @@
 
 #include "resource.h"
 
+#include <cfloat>
 #include <functional>
 #include <iostream>
 #include <linalg.h>
 #include <memory>
-#include <cfloat>
-
 
 
 using namespace linalg::aliases;
@@ -55,6 +54,8 @@ namespace cg::renderer
 			std::shared_ptr<resource<float>> in_depth_buffer)
 	{
 		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
+		if (in_render_target)
+			render_target = in_render_target;
 		// TODO Lab: 1.06 Adjust `set_render_target`, and `clear_render_target` methods of `cg::renderer::rasterizer` class to consume a depth buffer
 	}
 
@@ -62,6 +63,8 @@ namespace cg::renderer
 	inline void rasterizer<VB, RT>::set_viewport(size_t in_width, size_t in_height)
 	{
 		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
+		height = in_height;
+		width = in_width;
 	}
 
 	template<typename VB, typename RT>
@@ -69,6 +72,12 @@ namespace cg::renderer
 			const RT& in_clear_value, const float in_depth)
 	{
 		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
+		if (render_target)
+		{
+			for (size_t i = 0; i < render_target->get_number_of_elements(); i++) {
+				render_target->item(i) = in_clear_value;
+			}
+		}
 		// TODO Lab: 1.06 Adjust `set_render_target`, and `clear_render_target` methods of `cg::renderer::rasterizer` class to consume a depth buffer
 	}
 
