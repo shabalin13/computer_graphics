360ae35345b0a1c1b555b74b280c90312ca91b0d
Shabalin13 <d.shabalin@innopolis.university>
2022-09-23 00:18:06 +0300

    Adjust cg::renderer::rasterization_renderer class

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -13,7 +13,11 @@ void cg::renderer::rasterization_renderer::init()
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
 			settings->width, settings->height);
 	rasterizer->set_render_target(render_target);
+
 	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
+
 	// TODO Lab: 1.04 Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	// TODO Lab: 1.06 Add depth buffer in `cg::renderer::rasterization_renderer`
 }
@@ -22,6 +26,13 @@ void cg::renderer::rasterization_renderer::render()
 	// TODO Lab: 1.02 Implement image clearing & saving in `cg::renderer::rasterization_renderer` class
 	auto start = std::chrono::high_resolution_clock::now();
 	rasterizer->clear_render_target({111, 15, 112});
+
+	for (size_t shape_id = 0; shape_id < model->get_index_buffers().size(); shape_id++)
+	{
+		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
+		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
+		rasterizer->draw(model->get_index_buffers()[shape_id]->get_number_of_elements(), 0);
+	}
 
 	auto stop = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<float, std::milli> duration = stop - start;
