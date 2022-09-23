42dda3bf36f28ab68c6a20ed2815466f6fade6f6
Shabalin13 <d.shabalin@innopolis.university>
2022-09-24 01:53:39 +0300

    Add depth buffer in cg::renderer::rasterization_renderer

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -12,7 +12,6 @@ void cg::renderer::rasterization_renderer::init()
 	rasterizer->set_viewport(settings->width, settings->height);
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
 			settings->width, settings->height);
-	rasterizer->set_render_target(render_target);
 
 	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
 	model = std::make_shared<cg::world::model>();
@@ -34,6 +33,9 @@ void cg::renderer::rasterization_renderer::init()
 	camera->set_z_far(settings->camera_z_far);
 
 	// TODO Lab: 1.06 Add depth buffer in `cg::renderer::rasterization_renderer`
+	depth_buffer = std::make_shared<resource<float>>(settings->width, settings->height);
+
+	rasterizer->set_render_target(render_target, depth_buffer);
 }
 void cg::renderer::rasterization_renderer::render()
 {
