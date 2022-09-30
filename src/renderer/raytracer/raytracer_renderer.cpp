03c6e2704dc5810d0e15947a5211c9300802794d
Shabalin13 <d.shabalin@innopolis.university>
2022-09-30 16:20:21 +0300

    Define `any_hit_shader` and `miss_shader` for `shadow_raytracer`

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -78,6 +78,17 @@ void cg::renderer::ray_tracing_renderer::render()
 		return payload;
 	};
 
+	// TODO Lab: 2.04 Define `any_hit_shader` and `miss_shader` for `shadow_raytracer`
+	shadow_raytracer->miss_shader = [](const ray& ray) {
+		payload payload{};
+		payload.t = -1.f;
+		return payload;
+	};
+
+	shadow_raytracer->any_hit_shader = [](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle) {
+		return payload;
+	};
+
 	raytracer->build_acceleration_structure();
 	shadow_raytracer->build_acceleration_structure();
 
@@ -96,7 +107,6 @@ void cg::renderer::ray_tracing_renderer::render()
 	std::cout << "Raytracing took " << raytracing_duration.count() << " ms\n";
 
 	cg::utils::save_resource(*render_target, settings->result_path);
-	// TODO Lab: 2.04 Define `any_hit_shader` and `miss_shader` for `shadow_raytracer`
 	// TODO Lab: 2.04 Adjust `closest_hit_shader` of `raytracer` to cast shadows rays and to ignore occluded lights
 	// TODO Lab: 2.05 Adjust `ray_tracing_renderer` class to build the acceleration structure
 	// TODO Lab: 2.06 (Bonus) Adjust `closest_hit_shader` for Monte-Carlo light tracing
