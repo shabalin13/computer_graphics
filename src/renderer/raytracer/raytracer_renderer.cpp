9451c3a322c75ffd131db4d635d5799599501e9f
Shabalin13 <d.shabalin@innopolis.university>
2022-09-30 17:19:49 +0300

    Adjust `ray_tracing_renderer` class to build the acceleration structure

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -39,8 +39,6 @@ void cg::renderer::ray_tracing_renderer::init()
 
 	// TODO Lab: 2.04 Initialize `shadow_raytracer` in `ray_tracing_renderer`
 	shadow_raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
-	shadow_raytracer->set_vertex_buffers(model->get_vertex_buffers());
-	shadow_raytracer->set_index_buffers(model->get_index_buffers());
 }
 
 void cg::renderer::ray_tracing_renderer::destroy() {}
@@ -100,7 +98,8 @@ void cg::renderer::ray_tracing_renderer::render()
 	};
 
 	raytracer->build_acceleration_structure();
-	shadow_raytracer->build_acceleration_structure();
+	// TODO Lab: 2.05 Adjust `ray_tracing_renderer` class to build the acceleration structure
+	shadow_raytracer->acceleration_structures = raytracer->acceleration_structures;
 
 	auto start = std::chrono::high_resolution_clock::now();
 
@@ -118,6 +117,5 @@ void cg::renderer::ray_tracing_renderer::render()
 
 	cg::utils::save_resource(*render_target, settings->result_path);
 
-	// TODO Lab: 2.05 Adjust `ray_tracing_renderer` class to build the acceleration structure
 	// TODO Lab: 2.06 (Bonus) Adjust `closest_hit_shader` for Monte-Carlo light tracing
 }
