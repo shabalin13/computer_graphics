f0dd3330c4869c6175fbcbff954bca56c1649d69
Shabalin13 <d.shabalin@innopolis.university>
2022-09-30 14:45:20 +0300

    bug fix

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -176,7 +176,7 @@ namespace cg::renderer
 		for (int x = 0; x < width; x++)
 		{
 //#pragma omp parallel for
-			for (int y = 0; x < height; y++)
+			for (int y = 0; y < height; y++)
 			{
 				float u = (2.f * x) / static_cast<float>(width - 1) - 1.f;
 				float v = (2.f * y) / static_cast<float>(height - 1) - 1.f;
